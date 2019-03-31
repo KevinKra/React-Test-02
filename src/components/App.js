7ea695caaf573ce533cd49fe7d29d27752dc23c3
Kevin Krato <kratok08@gmail.com>
2019-03-30 18:15:37 -0600

    Add additional files

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="top-level-element">
+        <div className="page">
+          <header tagline="Hello World"></header>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
